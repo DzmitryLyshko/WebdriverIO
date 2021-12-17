a197f9298c29666705746d6210fdc179bed6b5ea
Dzmitry Lyshko <88776780+DzmitryLyshko@users.noreply.github.com>
2021-12-17 14:09:48 +0200

    Update files

--- test/specs/test2.js
+++ test/specs/test2.js
@@ -1,3 +1,5 @@
+expect = require('chai').expect;
+
 const PastebinPage = require('../../lib/pastebin_page');
 const pastebinPage = new PastebinPage();
 
@@ -8,39 +10,33 @@ describe('pastebin page scenarios', function () {
     git push origin master --force`;
   pasteNameText = 'helloweb';
   pasteNameText2 = 'how to gain dominance among developers';
+  expectedHighlighting = 'Bash';
 
   before(async function () {
     await pastebinPage.open();
     await pastebinPage.maximizeWindow();
-    // Confirm privacy policy
     await pastebinPage.confirmPrivacyPolicy();
-    //  Close the banner that doesn't allow to push the button
     await pastebinPage.closeAds();
-    // Создать New Paste со следующими деталями:
-    // * Код:
     await pastebinPage.enterNewPasteText();
-    // * Syntax Highlighting: "Bash"
     await pastebinPage.selectSyntaxHighlighting();
-    // * Paste Expiration: "10 Minutes"
     await pastebinPage.selectPasteExpiration();
-    // * Paste Name / Title: "how to gain dominance among developers"
     await pastebinPage.enterPasteName();
-    // 3. Сохранить paste:
     await pastebinPage.savePaste();
-    // Confirm privacy policy (just to see the content of the window) - doesn't pop up in Belarus
     await pastebinPage.confirmPrivacyPolicy();
   });
 
   it('Page title should be equal to Paste name', async function () {
-    expect(browser).toHaveTitle(pasteNameText2 + ' - Pastebin.com');
+    expect(await browser.getTitle()).to.include(pasteNameText2);
   });
 
   it('Syntax should be highlighted for Bash', async function () {
-    expect(await pastebinPage.actualSyntaxHighlighting()).toHaveText('Bash');
+    expect(await pastebinPage.actualSyntaxHighlighting()).to.equal(
+      expectedHighlighting
+    );
   });
 
   it('Code should be equal to that we have entered to the New Paste', async function () {
-    expect(await pastebinPage.pasteData()).toHaveText(newPasteText2);
+    expect(await pastebinPage.pasteData()).to.equal(newPasteText2);
   });
 
   after(async function () {
--- test/specs/test3.js
+++ test/specs/test3.js
@@ -1,3 +1,5 @@
+expect = require('chai').expect;
+
 const PricingCalculatorPage = require('../../lib/pricing_calculator_page');
 const pricingCalculatorPage = new PricingCalculatorPage();
 
@@ -7,72 +9,57 @@ describe('pricing calculator page scenarios', function () {
   before(async function () {
     await pricingCalculatorPage.open();
     await pricingCalculatorPage.maximizeWindow();
-    // Нажав кнопку поиска по порталу вверху страницы, ввести в поле поиска"Google Cloud Platform Pricing Calculator"
     await pricingCalculatorPage.enterSearchQuery();
-    // В результатах поиска кликнуть "Google Cloud Platform Pricing Calculator" и перейти на страницу калькулятора.
     await pricingCalculatorPage.choosePricingCalculator();
     await pricingCalculatorPage.confirmCookies();
-    // Switch to iFrame
     await pricingCalculatorPage.switchToIFrame();
-    // Заполнить форму следующими данными:
-    //     * Number of instances: 4
     await pricingCalculatorPage.enterNumberOfInstances();
-    //     * Instance type: n1-standard-8    (vCPUs: 8, RAM: 30 GB)
-    // Choose Series N1
     await pricingCalculatorPage.selectInstanceSeries();
-    // Choose Series n1-standard-8    (vCPUs: 8, RAM: 30 GB)
     await pricingCalculatorPage.selectInstanceMachineType();
-    //     * Выбрать Add GPUs
     await pricingCalculatorPage.scrollTo(
       pricingCalculatorPage.addToEstimateBtn
     );
     await pricingCalculatorPage.selectCheckboxAddGPUs();
-    //     * GPU type: NVIDIA Tesla V100
     await pricingCalculatorPage.selectGPUTypeDropdown();
-    //     * Number of GPUs: 1
     await pricingCalculatorPage.selectNumberOfGPUs();
-    //     * Local SSD: 2x375 Gb
     await pricingCalculatorPage.selectLocalSSD();
-    //     * Datacenter location: Frankfurt (europe-west3)
     await pricingCalculatorPage.selectDatacenterLocation();
-    //     * Commited usage: 1 Year
     await pricingCalculatorPage.selectCommitedUsage();
-    // Нажать Add to Estimate
     await pricingCalculatorPage.clickAddToEstimateBtn();
   });
 
   it('Verify the correspondence of the data of the following field: VM Class', async function () {
-    expect(await pricingCalculatorPage.actualVMClass()).toHaveText(
+    expect(await pricingCalculatorPage.getActualVMClass()).to.equal(
       'VM class: regular'
     );
   });
 
   it('Verify the correspondence of the data of the following field: Instance type', async function () {
-    expect(
-      await pricingCalculatorPage.actualInstanceType()
-    ).toHaveTextContaining('n1-standard-8');
+    expect(await pricingCalculatorPage.getActualInstanceType()).to.include(
+      'n1-standard-8'
+    );
   });
 
   it('Verify the correspondence of the data of the following field: Region', async function () {
-    expect(await pricingCalculatorPage.actualRegion()).toHaveText(
+    expect(await pricingCalculatorPage.getActualRegion()).to.equal(
       'Region: Frankfurt'
     );
   });
 
   it('Verify the correspondence of the data of the following field: local SSD', async function () {
-    expect(await pricingCalculatorPage.actualLocalSSD()).toHaveTextContaining(
+    expect(await pricingCalculatorPage.getActualLocalSSD()).to.include(
       'Local SSD: 2x375 GiB'
     );
   });
 
   it('Verify the correspondence of the data of the following field: commitment term', async function () {
-    expect(await pricingCalculatorPage.actualCommitmentTerm()).toHaveText(
+    expect(await pricingCalculatorPage.getActualCommitmentTerm()).to.equal(
       'Commitment term: 1 Year'
     );
   });
 
   it('Verify that the rental amount per month equals with the amount received when passing the test manually', async function () {
-    expect(await pricingCalculatorPage.actualEstimatedCost()).toHaveText(
+    expect(await pricingCalculatorPage.getActualEstimatedCost()).to.equal(
       'Total Estimated Cost: USD 1,082.77 per 1 month'
     );
   });
